7fd076d6a81949eb5658882ef096a0d78fe38968
lolikion <st117139@student.spbu.ru>
2023-10-14 23:15:26 +0300

    05.10.23

--- main.c
+++ main.c
@@ -5,11 +5,22 @@
 #include "big_int/big_int.h"
 #include <time.h>
 #include <stdlib.h>
-
+extern long con;
 int main() {
-    big_int *n1= big_int_rnd(400000000);
-//    big_int_print(n1);
-    big_int_free(n1);
+    tst();
+//    clock_t start_time, end_time;
+//    double total_time;
+//    start_time = clock();
+//    big_int *n1= big_int_rnd(10000);
+//    big_int *n2= big_int_rnd(10000);
+//    big_int *n3= big_int_mult(n1,n2);
+//    end_time = clock();
+//    total_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+//    printf("const=%ld Время выполнения: %f секунд\n",con,total_time);
+////    big_int_print(n1);
+//    big_int_free(n1);
+//    big_int_free(n2);
+//    big_int_free(n3);
 //    big_int *n1= big_int_get("0010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001111111111111111111111111111111111111111111111111111111100000000000000000000000000000000000000000000000");
 //    big_int *n2= big_int_get("10000000000000000000001111111111111111111111111111111111111111111111111100000000000000000000000");
 //    big_int *n3= big_int_get("1000000000000000000000000111111111111111111111111111111111111111111111111111111111111111111111100000000001");
