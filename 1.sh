7f0926cd14761b3f4befd6bd44bd3c21c328c9cc
lolikion <st117139@student.spbu.ru>
2023-10-16 17:20:50 +0300

    05.10.23

--- 1.sh
+++ 1.sh
@@ -1,3 +1,6 @@
-gcc -c main.c big_int/big_int.c lib/lib.c -march=native -Ofast
-gcc -o main main.o big_int.o lib.o -lm -march=native -Ofast
+
+clear
+
+gcc -c main.c big_int/big_int.c lib/lib.c tests.c -march=native -Ofast
+gcc -o main main.o big_int.o lib.o tests.o -lm
 ./main
---
+++ tests.h
@@ -0,0 +1,8 @@
+//
+// Created by lolikion on 16.10.23.
+//
+
+#ifndef UNTITLED_TESTS_H
+#define UNTITLED_TESTS_H
+void tst();
+#endif //UNTITLED_TESTS_H
