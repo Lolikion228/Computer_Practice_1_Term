6b62392d3825c7676cee708c2c52e1768a1f8719
lolikion <st117139@student.spbu.ru>
2023-10-20 20:53:11 +0300

    05.10.23

--- main.c
+++ main.c
@@ -9,26 +9,27 @@
 #include <stdarg.h>
 
 int main() {
-//    clock_t start_time, end_time;
-//    double total_time;
-//    start_time = clock();
-//    srand(time(NULL));
+    clock_t start_time, end_time;
+    double total_time;
+    start_time = clock();
+    srand(time(NULL));
 
-    tst();
-//    printf("%d\n", big_int_primality_test(big_int_get("100001"),20));
-//    big_int *n1=big_int_rnd(55556);
-//    big_int *n2=big_int_rnd(55556);
+//    tst();
 
 
-
-
+//
+//    big_int *n1= big_int_get_prime(200,10);
+//    big_int_print(n1);
 
 //    big_int_test_loop(10,big_int_primality_test);
 
 
+//    printf("%d\n", mul_inverse(4,5));
+    big_int_print(big_int_extended_eu(big_int_get("111"), big_int_get("10000")));
 
-//    end_time = clock();
-//    total_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
+
+    end_time = clock();
+    total_time = ((double) (end_time - start_time)) / CLOCKS_PER_SEC;
 //    printf("Время выполнения : %f секунд\n", total_time);
     return 0;
 
