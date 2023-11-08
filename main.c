e9e907793771122b3d74f82f8e899c95ba3540a5
lolikion <st117139@student.spbu.ru>
2023-11-08 20:12:17 +0300

    05.10.23

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ int main() {
 //    big_int_print(exp1);
 
     console_app();
-    printf("lolik\n");
+
 
 //    big_int *msg=char_to_big_int("prikol123 12409 prikol");
 //    rsa_key *public_key= RSA_key_get(24);
